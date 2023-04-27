26415fe8d580626f770d6be8ed8d9ce92190a970
Michal Zonis <michal2467300@gmail.com>
2023-04-28 02:10:04 +0300

    add to list

--- client/roommates/src/app/app.module.ts
+++ client/roommates/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +15,8 @@ import { ListComponent } from './components/grocery_list/list/list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
