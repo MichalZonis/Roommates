70326b453becfee3ab162358847cff25cfcd2c89
Michal Zonis <michal2467300@gmail.com>
2023-04-27 17:58:41 +0300

    app init

---
+++ server/Rommates/Rommates/Controllers/GroceriesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Rommates.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroceriesController : ControllerBase
+    {
+        // GET: api/<GroceriesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<GroceriesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GroceriesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GroceriesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GroceriesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
