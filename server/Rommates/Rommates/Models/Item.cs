9fe8e95b9ddba8ffc9a6cff24285cba4288e30f2
Michal Zonis <michal2467300@gmail.com>
2023-04-29 22:09:42 +0300

    orm

---
+++ server/Rommates/Rommates/Models/Item.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace Roommates.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ItemContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        protected readonly IConfiguration Configuration;
+
+        public ItemContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+    }
+
+
+}
