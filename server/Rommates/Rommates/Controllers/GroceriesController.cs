55a8f7377b499f3a5461a03f962124d8cb3a5126
Michal Zonis <michal2467300@gmail.com>
2023-04-29 17:01:46 +0300

    roommates rename

--- server/Rommates/Rommates/Controllers/GroceriesController.cs
+++ server/Rommates/Rommates/Controllers/GroceriesController.cs
@@ -2,7 +2,7 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Rommates.Controllers
+namespace Roommates.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
