91335fb41e33f0c59e1016c88838fd351b7ae3f8
Michal Zonis <michal2467300@gmail.com>
2023-05-05 00:38:03 +0300

    item logic

---
+++ server/Rommates/Rommates/Logic/ItemsLogic.cs
@@ -0,0 +1,16 @@
+﻿using Roommates.Models;
+
+namespace Roommates.Logic
+{
+    public class ItemsLogic
+    {
+        public static int addItem(Item newItem)
+        {
+            using (var db = new ItemContext())
+            {
+                db.Items.Add(newItem);
+                return db.SaveChanges();
+            }
+        }
+    }
+}
