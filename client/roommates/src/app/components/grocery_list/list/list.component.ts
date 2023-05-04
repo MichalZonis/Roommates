d7205b23285ba9bdf70bc40bfd6b1e2e7dc87634
Michal Zonis <michal2467300@gmail.com>
2023-05-05 00:38:46 +0300

    list

--- client/roommates/src/app/components/grocery_list/list/list.component.ts
+++ client/roommates/src/app/components/grocery_list/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Item } from 'src/app/models/Item';
+import { ItemsService } from 'src/app/services/items.service';
 
 @Component({
   selector: 'app-list',
@@ -11,14 +13,16 @@ export class ListComponent implements OnInit {
   list: string[] = []
   newItem = new FormControl('')
 
-  constructor() { }
+  constructor(private s_items: ItemsService) { }
 
   ngOnInit(): void {
+    this.s_items.items.subscribe((item: any) => this.list.push(item.name))
   }
 
   addItem(item: string | null) {
-    if (item != null) this.list.push(item);
-    this.newItem.reset();
+    if (item != null) {
+      this.s_items.addItem(new Item(item))
+      this.newItem.reset();
+    }
   }
-
 }
---
+++ server/Rommates/Rommates/Models/List.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Roommates.Models
+{
+    public class List
+    {
+        public int Id { get; set; }
+        public Item[] Items { get; set; }
+    }
+
+
+    public class ListContext : DbContext
+    {
+        public DbSet<List> Lists { get; set; }
+        protected readonly IConfiguration Configuration;
+
+        public ListContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+    }
+}
